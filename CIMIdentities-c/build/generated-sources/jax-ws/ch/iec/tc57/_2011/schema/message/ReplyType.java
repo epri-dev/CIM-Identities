b776992893be9f73c0695a4a9af5ef53f11b1275
Daniel Lowe <Xynthion@gmail.com>
2017-06-09 11:20:43 -0400

    Added "View Database" page/bug fixes

--- CIMIdentities-c/build/generated-sources/jax-ws/ch/iec/tc57/_2011/schema/message/ReplyType.java
+++ CIMIdentities-c/build/generated-sources/jax-ws/ch/iec/tc57/_2011/schema/message/ReplyType.java
@@ -175,8 +175,8 @@ public class ReplyType {
      * 
      * <p>
      * Objects of the following type(s) are allowed in the list
-     * {@link Element }
      * {@link Object }
+     * {@link Element }
      * 
      * 
      */
--- CIMIdentities-c/build/generated/jax-wsCache/SendCIMIdentities/ch/iec/tc57/_2011/schema/message/HeaderType.java
+++ CIMIdentities-c/build/generated/jax-wsCache/SendCIMIdentities/ch/iec/tc57/_2011/schema/message/HeaderType.java
@@ -506,8 +506,8 @@ public class HeaderType {
      * 
      * <p>
      * Objects of the following type(s) are allowed in the list
-     * {@link Object }
      * {@link Element }
+     * {@link Object }
      * 
      * 
      */
