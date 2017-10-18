fa949fd198485db1ea9b760e35214e9b06830e80
Vail, Sean <svail@epri.com>
2017-10-18 12:48:00 -0700

    Uploading version 1.0

--- CIMIdentities-w/build/generated/jax-wsCache/service/GetCIMIdentities/com/epri/_2016/cimidentities_/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.epri.com/2016/CIMIdentities#", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.epri._2016.cimidentities_;
